f139f09c5cce41b0fbdd2546468359752b5fad9c
Ashmit Kumar Mishra <93040270+ashmitKmishra@users.noreply.github.com>
2025-11-27 15:42:14 -0600

    Don't push api keys
    Replaced hardcoded API key with a placeholder.

--- list_models.py
+++ list_models.py
@@ -1,6 +1,7 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyAO45PdsYOuOZliIY93FP7YC0vj5UJiZBU")
+client = genai.Client(api_key=your-key)
 
 for model in client.models.list():
     print(model.name)
+
